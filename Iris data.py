f28488d743b6f4e5f6e558ce0b1824168d39d1ec
Ann311 <67332880+Ann311@users.noreply.github.com>
2020-07-27 13:03:30 +0530

    Update Iris data.py

--- Iris data.py
+++ Iris data.py
@@ -1,34 +1,34 @@
 #importing the Dataset
-import pandas as pd
-data=pd.read_csv('Iris.csv')
+import pandas as pd    #import pandas package for importing dataset
+data=pd.read_csv('Iris.csv')   #it reads the dataset & assume the dataset in a variable.  read_csv is only read csv files
 #cleaning the data
-data.drop('Id',inplace=True,axis=1)
-data['Species'].unique()
-classes={'Iris_setosa':0,'Iris_versicolor':1,'Iris_virginica':2}
-data.replace({'Species':classes},inplace=True)
+data.drop('Id',inplace=True,axis=1)     #deleting the unwanted data in the dataset.In drop() first mention the name,inplace=True implies it is present,axis=1 implies column
+data['Species'].unique()                #sets unique 
+classes={'Iris_setosa':0,'Iris_versicolor':1,'Iris_virginica':2}   
+data.replace({'Species':classes},inplace=True)          #replace the species name as in the classes dictionary
 #visualizing the data
-import seaborn as sb
-sb.pairplot(data,hue='Species',markers=['o','s','D'])
-#Spliting the data
-x=data.iloc[:,:-1].values
-y=data.iloc[:,-1].values
+import seaborn as sb                                  #importing seaborn package for visualization
+sb.pairplot(data,hue='Species',markers=['o','s','D'])   #pairplot is visualization technique.In pairplot() first one is the name of the dataset,hue=species implies which is the target,markers=[o,s,D] implies the shape 
+#Spliting the data into x & y
+x=data.iloc[:,:-1].values         #split the data into x by avoiding last column.
+y=data.iloc[:,-1].values          #split the data have only last column  
 #spliting the data into training and testing
-from sklearn.model_selection import train_test_split as tts
-xtrain,xtest,ytrain,ytest=tts(x,y,test_size=0.25,random_state=11)
+from sklearn.model_selection import train_test_split as tts    #importing train_test_split package for training and testing
+xtrain,xtest,ytrain,ytest=tts(x,y,test_size=0.25,random_state=11)  # split the x and y into testing and training
 #normalizing the data
-from sklearn.preprocessing import StandardScaler as sc
-sca=sc()
-sca.fit(xtrain)
-xtrain=sca.transform(xtrain)
-xtest=sca.transform(xtest)
+from sklearn.preprocessing import StandardScaler       #import StandardScaler for normalizing the data
+sca=StandardScaler()     #object created for the package
+sca.fit(xtrain)         #fitting the xtrain data
+xtrain=sca.transform(xtrain)      #transforming the training data
+xtest=sca.transform(xtest)       
 #Algorithm Selection
 #Logistic Regression
-from sklearn.linear_model import LogisticRegression as lr
-mlr=lr()
-mlr.fit(xtrain,ytrain)
-mlr.score(xtest,ytest)
+from sklearn.linear_model import LogisticRegression     #algorithm LogisticRegression is imported for the data
+mlr=LogisticRegression()   
+mlr.fit(xtrain,ytrain)    #fitting the training data
+mlr.score(xtest,ytest)    #accuracy of the data after learning 
 #KNeighbors Classifier
-from sklearn.neighbors import KNeighborsClassifier as knn
-mknn=knn()
-mknn.fit(xtrain,ytrain)
+from sklearn.neighbors import KNeighborsClassifier    #algorithm KNeighborsClassifier is imported for the data
+mknn=KNeighborsClassifier()   
+mknn.fit(xtrain,ytrain) 
 mknn.score(xtest,ytest)
